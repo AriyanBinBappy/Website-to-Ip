99e5935ac8b000545e8f9beccf42427569bb017d
AriyanBinBappy <ariyanbinbappy2367@gmail.com>
2025-06-15 13:45:55 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+
+---
+
+## 🔧 Requirements
+
+- Python 3.x
+
+---
+
+## 📦 Installation
+
+```bash
+git clone https://github.com/yourusername/ip-extractor.git
+cd ip-extractor
+python3 ip_extractor.py
